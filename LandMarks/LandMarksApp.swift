7bc7060c48fa85fec027040b179bb7c249979ab0
Meet Madhu <meetmadhu1310@gmail.com>
2024-06-16 17:09:35 -0700

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Meet Madhu on 2024-06-16.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
